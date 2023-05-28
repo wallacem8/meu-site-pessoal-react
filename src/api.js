052fdac50f1968f60d35d670600246dadaac6588
Wallace Medeiros <wallacemedeiros_97@hotmail.com>
2023-05-28 00:20:18 -0300

    adicionando em nuvem, onrender.

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://json-server-ex-v27k.onrender.com"
+    baseURL: "https://site-pessoal-api-4r1s.onrender.com/api"
 });
 
 export default api;
